f08fe3f097b2992ea889fd9006d65b8a3dc176cf
Christian Lagelstorfer <wi19b076@technikum-wien.at>
2020-11-05 10:17:59 +0100

    Projektdateien hinzufügen.

---
+++ Calcy/SpecFlowFeature1.feature
@@ -0,0 +1,19 @@
+﻿Feature: Trigonometry
+	In order to answer difficult trigonometric questions
+	As a math idiot
+	I want to be told the correct calculation for Pi, Sin, Cos, Tan
+
+Scenario: calculate sinus
+	Given a number is Pi
+	When the sinus is calculated
+	Then the result should be 0
+
+Scenario: calculate cosinus
+	Given a number is Pi
+	When the cosinus is calculated
+	Then the result should be -1
+
+Scenario: calculate tangens
+	Given a number is Pi
+	When the tangens is calculated
+	Then the result should be 0
---
+++ Calcy/TrigonometrySteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Calcy
+{
+    [Binding]
+    public class TrigonometrySteps
+    {
+        private double x, y;
+        [Given(@"a number is Pi")]
+        public void GivenANumberIsPi()
+        {
+            x = Math.PI;
+        }
+        
+        [When(@"the sinus is calculated")]
+        public void WhenTheSinusIsCalculated()
+        {
+            y = Math.Sin(x);
+        }
+        
+        [When(@"the cosinus is calculated")]
+        public void WhenTheCosinusIsCalculated()
+        {
+            y = Math.Cos(x);
+        }
+
+        [When(@"the tangens is calculated")]
+        public void WhenTheTangensIsCalculated()
+        {
+            y = Math.Tan(x);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            var expected = p0;
+            var actual = y;
+            Assert.Equal(expected,actual,5);
+        }
+    }
+}
